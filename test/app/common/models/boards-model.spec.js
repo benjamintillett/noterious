b69db35a9c6697b8dcf1d406908ae0c52f1cf600
benjamin tillett <benjamin.tillett@gmail.com>
2015-09-05 09:20:57 +0100

    add first boardSpecs

--- test/app/common/models/boards-model.spec.js
+++ test/app/common/models/boards-model.spec.js
@@ -1,15 +1,47 @@
 'use strict';
 
 describe("Service: BoardsModel", function() {
-  beforeEach(module('noterious'));
-
   var BoardsModel;
+  var $httpBackend; 
+  var UserModelMock;
+  var currentUserId = 745;
+  
+  beforeEach(function(){
+  	module('noterious',function($provide){
+
+  		UserModelMock = {
+  			getCurrentUser: function(){
+  				return 1
+  			}
+  		}
+  		spyOn(UserModelMock,"getCurrentUser").and.returnValue(currentUserId);
+
+  		$provide.value('UserModel',UserModelMock);
+  	});	
+  })
+
+
 
   beforeEach(inject(function(_BoardsModel_) {
     BoardsModel = _BoardsModel_;
   }));
 
-  it('should be defined', function() {
-    expect(BoardsModel).toBeDefined();
+  beforeEach(inject(function($injector){
+  	$httpBackend = $injector.get('$httpBackend');
+  	 $httpBackend.whenGET(/app.*/).respond(200, '');
+  }));
+
+  describe('.all',function(){
+	  it('requests the current user', function() {
+	    BoardsModel.all();
+	    expect(UserModelMock.getCurrentUser).toHaveBeenCalled();
+	  });
+
+	 	it('correctly requests the users boards from the server', function() {
+	    BoardsModel.all()
+	    $httpBackend.expectGET(ENDPOINT_URI + 'users/' + currentUserId + '/boards.json').respond(200, '');
+	    $httpBackend.flush();
+	  });
   });
+
 });
